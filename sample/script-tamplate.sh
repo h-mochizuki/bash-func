0fec2b3a4151d1109091733591dc7672048a64b6
hidetoshi.mochizuki <hidetoshi.mochizuki@ulsystems.co.jp>
2017-05-18 19:01:58 +0900

    サンプル修正

--- sample/script-tamplate.sh
+++ sample/script-tamplate.sh
@@ -6,42 +6,61 @@ base="$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)"
 self="$(basename ${BASH_SOURCE:-$0})"
 
 # -----------
-#  Functions
+#   変数定義
+# -----------
+# 入力引数
+args=()
+
+# -----------
+#   関数定義
 # -----------
 function _usage() {
-  # usage
+  # 使用方法
   cat <<USAGE >&2
 
 usage)
-  ./${self} [options] [parameters: p1, ...]
+  ./${self} [options: -h] [parameters: text, ...]
 options)
   -h : Show this usage.
 params)
-  p1 : Please write description.
+  text : Please input text.
+         (Default: Current-date)
 USAGE
 }
 
 function main() {
-  # Main
+  # メイン処理
   echo "$@"
 }
 
-# -----------
-#  Processes
-# -----------
-# =Validate & Setting Options
+# --------------
+#  入力チェック
+# --------------
+# オプションを設定します
 while getopts h OPT
 do
   case $OPT in
-    h) _usage
-       exit 0
+    h) # ヘルプ
+      _usage
+      exit 0
     ;;
-    \?) echo -en "\e[31m" # red color.
-        _usage
-        exit 1
+    \?) # 想定外のオプション
+      echo -en "\e[31m" # 赤くする
+      _usage
+      exit 1
   esac
 done
 shift $((OPTIND - 1))
 
-# =Main Start
-main "$@" &2>1
+# 引数チェック
+if [[ "$#" -eq 0 ]];then
+  echo -e "\e[31m引数がありません。現在日付を代入します\e[m"
+  args=( "$(date +'%Y-%m-%d')" "$(date +'%H:%M:%S')" )
+else
+  args=( "$@" )
+fi
+
+# --------------
+#  メイン処理実行
+# --------------
+main "${args[@]}" &2>1
